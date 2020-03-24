16c92ce570c87a23816cbb3794dcc048eff701c7
joris <jorisvddonk@gmail.com>
2020-03-24 15:46:13 +0100

    planet pages

--- createPages.ts
+++ createPages.ts
@@ -13,17 +13,37 @@ export default async ({ graphql, actions }) => {
           }
         }
       }
+      allPlanet {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
     }
   `);
-    result.data.allStar.edges.forEach(({ node }) => {
-        createPage({
-            path: node.fields.slug,
-            component: path.resolve(`./src/templates/StarInfo.tsx`),
-            context: {
-                // Data passed to context is available
-                // in page queries as GraphQL variables.
-                slug: node.fields.slug
-            }
-        });
+  result.data.allStar.edges.forEach(({ node }) => {
+    createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/StarInfo.tsx`),
+        context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            slug: node.fields.slug
+        }
     });
+});
+result.data.allPlanet.edges.forEach(({ node }) => {
+  createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/PlanetInfo.tsx`),
+      context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug
+      }
+  });
+});
 }
---
+++ src/templates/PlanetInfo.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { DefaultLayout as Layout } from "../layouts/index";
+import { graphql } from "gatsby";
+
+export default function PlanetInfo({ data }) {
+  const { planet } = data;
+  return (
+    <Layout>
+      <h1>{planet.data.name}</h1>
+      <p>
+        planet with index <em>{planet.data.index}</em> at {planet.data.x}, {planet.data.y},{" "}
+        {planet.data.z}
+      </p>
+
+      <h2>Guide entries</h2>
+      <pre>
+        {planet.childrenGuideEntry.map(entry => entry.data.text).join("\n")}
+      </pre>
+    </Layout>
+  );
+}
+export const query = graphql`
+  query($slug: String!) {
+    planet(fields: { slug: { eq: $slug } }) {
+      id
+      data {
+        name
+        index
+        x
+        y
+        z
+      }
+      childrenGuideEntry {
+        data {
+          text
+        }
+      }
+    }
+  }
+`;
